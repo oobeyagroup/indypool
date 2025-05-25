004b35a41013239fa4ce703f52d3a02344dd89ed
lowelllindstrom <lowell@oobeyagroup.com>
2025-05-24 20:49:31 -0400

    Update for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn indypool.wsgi --log-file -
--- indypool/settings.py
+++ indypool/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g=9ds@iqyd7u@k!n#13gt@$+bwn$xd9xscb+qb^m1_eam^voal'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-g=9ds@iqyd7u@k!n#13gt@$+bwn$xd9xscb+qb^m1_eam^voal')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*'] if DEBUG else ['indypool.herokuapp.com']
 
 # Application definition
 
@@ -37,10 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -58,6 +59,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -70,13 +72,9 @@ WSGI_APPLICATION = 'indypool.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
